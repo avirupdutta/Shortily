c811843bbfa64007cd4786ba23737afda6139c87
avirupdutta <finalguider99@gmail.com>
2020-09-21 09:26:39 +0530

    ready for deployment

--- db/dbConfig.ts
+++ db/dbConfig.ts
@@ -1,3 +1,13 @@
-const database: string = `mongodb://localhost:27017/Shorty?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
+import settings from "../settings";
+
+let database: string = "";
+
+if (process.env.ENV_TYPE === settings.ENV.dev) {
+    database = `mongodb://localhost:27017/Shorty?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
+} else if (process.env.ENV_TYPE === settings.ENV.prod) {
+    database = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.fqeoa.mongodb.net/${
+        process.env.MONGO_DB_NAME
+    }?retryWrites=true&w=majority`;
+}
 
 export default database;
---
+++ settings.ts
@@ -0,0 +1,8 @@
+const settings = {
+    ENV: {
+        dev: "dev",
+        prod: "production",
+    },
+};
+
+export default settings;
